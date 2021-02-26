776013ee74c37a6e889807b259f5d5b3e9656caa
njazgunovich <nicholasjaz@gmail.com>
2021-02-26 15:54:39 -0500

    fixed errors

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+let connection;
+if (process.env.JAWSDB_MARIA_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL)
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'oldauth',
-    // NOTE: Be sure to add your MySQL password here!
-    password: 'Seagirt8715$',
-    database: 'burger_db',
-});
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'oldauth',
+        // NOTE: Be sure to add your MySQL password here!
+        password: 'Seagirt8715$',
+        database: 'burger_db',
+    });
+}
 
 // Make connection.
 connection.connect((err) => {
